80a8fa6372dce3bf1990257c227bd4492a511873
osoman2 <osmancampeon69@hotmail.com>
2019-09-17 18:43:05 -0500

    iteradores

--- libreria/main.cpp
+++ libreria/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include "./ForwardList.h"
 #include "./DoubleList.h"
-/*#include "./CircularList.h"
-#include "./DoubleCircularList.h"*/
+#include "./CircularList.h"
+/*#include "./DoubleCircularList.h"*/
 using namespace std;
 
 int main()
-{
+{ int a_[] = {1,2,3,4};
   ForwardList<int> *a = new ForwardList<int>;
   DoubleList<int> *b = new DoubleList<int>;
-  /*CircularList<int> *c = new CircularList<int>;
-  DoubleCircularList<int> *d = new DoubleCircularList<int>;*/
+  CircularList<int> *c = new CircularList<int>(a_,4);
+  
   a->push_back(1);
   a->push_back(2);
   a->push_back(3);
@@ -38,5 +38,20 @@ int main()
   //cout << b->empty();
   //b->reverse();
   //cout << b->back() << endl;
+  cout<<"Circularlist:\n";
+  cout<< c->back()<<endl;
+  cout<< c->front()<<endl;
+  c->push_back(19);c->push_front(20);
+  cout<< c->back()<<endl;
+  cout<< c->front()<<endl;
+  *c<<21;*c>>22;
+cout<< c->back()<<endl;
+  cout<< c->front()<<endl;
+  c->clear();
+  cout<< c->back()<<endl;
+  cout<< c->front()<<endl;
+  delete a;
+  delete b;
+  delete c;
   return 0;
 }
